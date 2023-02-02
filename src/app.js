dfcc05b69d4ea480d8a13893e310359c016b182c
Csajbok-Remenyi Laszlo <csajbokremenyilaszlo@ktch.hu>
2023-02-02 20:18:18 +0100

    Változók/típusok gyakorlása

---
+++ src/app.js
@@ -0,0 +1,22 @@
+var szam = '35';
+var szam2 = '27';
+
+console.log(Number(szam) + Number(szam2));
+
+
+var calcArea=function (base, height){
+
+    return base*height/2;
+}
+
+console.log(typeof(calcArea));
+ var lista= {
+    nev: 'Pisti',
+    kor: 28,
+    kocsi: true
+ };
+console.log(lista.nev);
+
+var tomb = [25, 30, 40];
+console.log(typeof tomb);
+
